4e9a38b7843410c5176c01a2d37e0da309e26bdd
beauseant <suscripcion@bluethinking.com>
2014-03-05 17:48:38 +0100

    Clase para autenticar contra un servidor LDAP

--- classes/class.InventoryAuth.php
+++ classes/class.InventoryAuth.php
@@ -1,14 +1,64 @@
-i<?php
+<?php
 
 
-//require_once ("");
+	class InventoryAuth {
+	
+		private $server;
+		private $bind;
+		private $group;
+		private $groupadmin;
+		private $ds;
+	
+	
+		//user bind is something like uid = --login--, ou=People, dc=tsc, dc=uc3m,dc=es
+		//replace --login-- with de real login when connect:
+		function __construct( $server, $bind, $group, $groupadmin ) {
+			$this -> server 	= $server;
+			$this -> bind 		= $bind;
+			$this -> group 		= $group;
+			$this -> groupadmin 	= $groupadmin;
+		}
+		
+		
+	
+		
+		function connect ( $login, $passwd ) {
+			
+			if(!function_exists('ldap_connect')) {
+				echo 'Function ldap_connect not found. Try install php5_ldapmodule. Maybe: yum install php-ldap';
+				exit ();
+			}
+			
+			$this -> ds 	= ldap_connect( $this -> server ) or die("Could not connect to {$this -> server}");
 
 
-  
-class InventoryAuth {
+			$userbind = str_replace ( '--login--', $login, $this -> bind );		
 
-}
+			
+			$login = ldap_bind( $this -> ds, $userbind, $passwd );			
+		
+			return $login;
 
+		}
+		
+
+		function userInGroup ( $login, $passwd, $group ) {
+		
+			$salida = -1;
+			if ( $this -> connect ( $login, $passwd ) ) {
+				$sr=ldap_search($this -> ds,$group, "memberuid=$login");
+				$salida =  ldap_count_entries($this -> ds,$sr) ;
+			}
+			//-1 not valid user (passwd, login)
+			// 0 not in group
+			// > 0 user in group
+			return $salida;
+		
+		}	
+
+
+
+	}
 
 ?>
 
